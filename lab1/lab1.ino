e0d2ede7276189d4fa681d8b2c5f15f1af9feeaf
Denis Chernushchenko <NightQnEarth@gmail.com>
2019-10-19 18:29:19 +0500

    Complete task №1.

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,37 +1,29 @@
 #include <Arduino.h>
-#include <MD_TCS230.h>
 #include "button.h"
-
-#define  S0_OUT  2
-#define  S1_OUT  3
-#define  S2_OUT  4
-#define  S3_OUT  5
 
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
+#define INCREASE_R_BUTTON 9
+#define DECREASE_R_BUTTON 10
+#define INCREASE_G_BUTTON 11
+#define DECREASE_G_BUTTON 12
+#define INCREASE_B_BUTTON 13
+#define DECREASE_B_BUTTON 14
+#define COLOR_DELTA 50
 
-MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
+Button IncreasingButtons[] = { Button(INCREASE_R_BUTTON), Button(INCREASE_G_BUTTON), Button(INCREASE_B_BUTTON)};
+Button DecreasingButtons[] = { Button(DECREASE_R_BUTTON), Button(DECREASE_G_BUTTON), Button(DECREASE_B_BUTTON)};
+int RgbValues[] = { 0, 0, 0 };
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Started!");
-
-    sensorData whiteCalibration;
-    whiteCalibration.value[TCS230_RGB_R] = 0;
-    whiteCalibration.value[TCS230_RGB_G] = 0;
-    whiteCalibration.value[TCS230_RGB_B] = 0;
-
-    sensorData blackCalibration;
-    blackCalibration.value[TCS230_RGB_R] = 0;
-    blackCalibration.value[TCS230_RGB_G] = 0;
-    blackCalibration.value[TCS230_RGB_B] = 0;
-
-    colorSensor.begin();
-    colorSensor.setDarkCal(&blackCalibration);
-    colorSensor.setWhiteCal(&whiteCalibration);
-
+    pinMode(INCREASE_R_BUTTON, INPUT);
+    pinMode(INCREASE_G_BUTTON, INPUT);
+    pinMode(INCREASE_B_BUTTON, INPUT);
+    pinMode(DECREASE_R_BUTTON, INPUT);
+    pinMode(DECREASE_G_BUTTON, INPUT);
+    pinMode(DECREASE_B_BUTTON, INPUT);
     pinMode(R_OUT, OUTPUT);
     pinMode(G_OUT, OUTPUT);
     pinMode(B_OUT, OUTPUT);
@@ -39,29 +31,21 @@ void setup()
 
 void loop() 
 {
-    colorData rgb;
-    colorSensor.read();
+    for (int i = 0; i < 3; ++i)
+    {
+        if (IncreasingButtons[i].wasPressed())
+            RgbValues[i] = min(RgbValues[i] + COLOR_DELTA, 250);
 
-    while (!colorSensor.available());
+        if (DecreasingButtons[i].wasPressed())
+            RgbValues[i] = max(0, RgbValues[i] - COLOR_DELTA);
+    }
 
-    colorSensor.getRGB(&rgb);
-    print_rgb(rgb);
-    set_rgb_led(rgb);
+    set_rgb_led();
 }
 
-void print_rgb(colorData rgb)
+void set_rgb_led()
 {
-  Serial.print(rgb.value[TCS230_RGB_R]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_G]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_B]);
-  Serial.println();
-}
-
-void set_rgb_led(colorData rgb)
-{
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    analogWrite(R_OUT, RgbValues[0]);
+    analogWrite(G_OUT, RgbValues[1]);
+    analogWrite(B_OUT, RgbValues[2]);
 }
