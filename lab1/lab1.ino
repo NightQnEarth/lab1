7de08d34b45621a45545784fa838875e395eb964
Denis Chernushchenko <NightQnEarth@gmail.com>
2019-10-26 20:46:51 +0500

    Fixed bug with button's pinMode(), changed defined pins. Added README.

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,29 +1,22 @@
-#include <Arduino.h>
 #include "button.h"
 
-#define R_OUT 6
-#define G_OUT 7
-#define B_OUT 8
-#define INCREASE_R_BUTTON 9
-#define DECREASE_R_BUTTON 10
-#define INCREASE_G_BUTTON 11
-#define DECREASE_G_BUTTON 12
-#define INCREASE_B_BUTTON 13
-#define DECREASE_B_BUTTON 14
+#define R_OUT 5
+#define G_OUT 6
+#define B_OUT 7
+#define INCREASE_R_BUTTON 8
+#define DECREASE_R_BUTTON 9
+#define INCREASE_G_BUTTON 10
+#define DECREASE_G_BUTTON 11
+#define INCREASE_B_BUTTON 12
+#define DECREASE_B_BUTTON 13
 #define COLOR_DELTA 50
 
-Button IncreasingButtons[] = { Button(INCREASE_R_BUTTON), Button(INCREASE_G_BUTTON), Button(INCREASE_B_BUTTON)};
-Button DecreasingButtons[] = { Button(DECREASE_R_BUTTON), Button(DECREASE_G_BUTTON), Button(DECREASE_B_BUTTON)};
+Button IncreasingButtons[] = { Button(INCREASE_R_BUTTON), Button(INCREASE_G_BUTTON), Button(INCREASE_B_BUTTON) };
+Button DecreasingButtons[] = { Button(DECREASE_R_BUTTON), Button(DECREASE_G_BUTTON), Button(DECREASE_B_BUTTON) };
 int RgbValues[] = { 0, 0, 0 };
 
 void setup()
 {
-    pinMode(INCREASE_R_BUTTON, INPUT);
-    pinMode(INCREASE_G_BUTTON, INPUT);
-    pinMode(INCREASE_B_BUTTON, INPUT);
-    pinMode(DECREASE_R_BUTTON, INPUT);
-    pinMode(DECREASE_G_BUTTON, INPUT);
-    pinMode(DECREASE_B_BUTTON, INPUT);
     pinMode(R_OUT, OUTPUT);
     pinMode(G_OUT, OUTPUT);
     pinMode(B_OUT, OUTPUT);
